a78bbb6e0735ce90191eb02d9c5274f0ca8338ca
Mohamed Dhia <benrabouchmd@gmail.com>
2024-01-13 21:59:15 +0100

    Handling games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,11 @@
+import noImage from "../assets/No-Image-Placeholder.svg";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
+
   const target = "media/";
   const index = url.indexOf(target) + target.length;
+
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
